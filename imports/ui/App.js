e22ee85aa14349a52936520a59aece1633431120
OnoprienkoArtem <o.artem.e@gmail.com>
2018-11-06 23:26:54 +0200

    add count abilyty

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -61,7 +61,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
 
           <label className="hide-completed">
             <input
@@ -96,5 +96,10 @@ export default withTracker(() => {
         createdAt: -1
       }
     }).fetch(),
+    incompleteCount: Tasks.find({
+      checked: {
+        $ne: true
+      }
+    }).count(),
   };
 })(App);
