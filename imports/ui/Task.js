43defbeab798589bd9a8a027e10e5375f0c5284e
OnoprienkoArtem <o.artem.e@gmail.com>
2018-11-16 23:02:34 +0200

    install classnames

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 import { Tasks } from '../api/tasks.js';
 
@@ -21,8 +22,11 @@ export default class Task extends Component {
 
     render() {
         // Give tasks a different className when they are checked off,
-        // so that we can style them nicely in CSS
-        const taskClassName = this.props.task.checked ? 'checked' : '';
+        // so that we can style them nicely in CSS  
+        const taskClassName = classnames({
+          checked: this.props.task.checked,
+          private: this.props.task.private,
+        });
 
         return ( 
             <li className={taskClassName}>
