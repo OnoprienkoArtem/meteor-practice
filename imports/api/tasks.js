18c67948aa6cc9095f04fdb03eb7ba4350dfa570
OnoprienkoArtem <o.artem.e@gmail.com>
2018-11-24 22:45:22 +0200

    add test files with first test

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,12 +1,6 @@
-import {
-    Meteor
-} from 'meteor/meteor';
-import {
-    Mongo
-} from 'meteor/mongo';
-import {
-    check
-} from 'meteor/check';
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
